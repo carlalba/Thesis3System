93e26a636172c41c9227613743f5cdd856fbea2e
carlalba <90944102+carlalba@users.noreply.github.com>
2026-09-16 10:56:16 +0800

    Update app.py

--- Proof of Concept/app.py
+++ Proof of Concept/app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 # Load FULL pipelines (contain scaler + PCA + model)
 svm_grid = joblib.load("saved_models/Grid-SVM_final_run1.pkl")
 svm_ga = joblib.load("saved_models/GA-SVM_final_run1.pkl")
-
-# gcForest uses PCA separately
 pca_gc = joblib.load("saved_models/pca_final_run1.pkl")
 gc_forest = joblib.load("saved_models/gcForest_final_run1.pkl")
 
@@ -33,20 +31,12 @@ def predict():
         data = request.json["features"]
         X = np.array(data).reshape(1, -1)
 
-        # -------------------------------
-        # GRID-SVM and GA-SVM require NO MANUAL preprocess
-        # the pipeline handles:
-        # scaler → PCA → model
-        # -------------------------------
         pred_grid = svm_grid.predict(X)[0]
         pred_ga   = svm_ga.predict(X)[0]
 
         conf_grid = get_confidence(svm_grid, X)
         conf_ga   = get_confidence(svm_ga, X)
-
-        # -------------------------------
-        # gcForest requires PCA manually (only gcForest)
-        # -------------------------------
+        
         X_pca_gc = pca_gc.transform(X)
         pred_gc  = gc_forest.predict(X_pca_gc)[0]
         conf_gc  = get_confidence(gc_forest, X_pca_gc)
